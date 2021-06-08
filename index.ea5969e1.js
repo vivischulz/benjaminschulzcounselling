cd2a9548d6152083827159a4b2c6bd20f995a6a3
Vivi Schulz <haweirh@gmail.com>
2021-06-07 21:02:53 -0700

    build production.

---
+++ index.ea5969e1.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".collapsible").forEach((e=>e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
+//# sourceMappingURL=index.ea5969e1.js.map
