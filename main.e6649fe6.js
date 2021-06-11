960ef001d155e70fbc9874744b1cd867da554d3c
Vivi Schulz <haweirh@gmail.com>
2021-06-11 10:51:27 -0700

    delete main root.

--- main.e6649fe6.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".collapsible").forEach((e=>e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
-//# sourceMappingURL=main.e6649fe6.js.map
