904af41fd65143d91e14fcc44a7bd731eac1dc6f
Vivi Schulz <haweirh@gmail.com>
2021-06-11 14:44:59 -0700

    try fix contact form.

--- mail.php
+++ mail.php
@@ -1,25 +1,20 @@
 <?php
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-$visitor_phone=$_POST['phone'];
-$message=$_POST['message'];
+if(isset($_POST['submit'])){
+$name = $_POST['name'];
+$mailFrom = $_POST['mail'];
+$phoneFrom = $_POST['phone'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-$email_from='benjaminschulzcounselling.com';
 
-$email_subject="Counselling Inquiry";
 
-$email_body="User Name:$name.\n".
-                "User Email:$visitor_email.\n". 
-                "User Phone:$visitor_phone.\n". 
-                "User Message:$message.\n";
+$mailTo = "Benjamin.Schulz@hotmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail".$name.".\n\n".$message;
 
-$to="benjaminschulzcounselling@gmail.com";
+mail($mailTo, $subject, $text, $headers);
+header("Location: index.html?mailsend");
+}
 
-$headers="From:$email_from \r\n";
 
-$headers .="Reply-To: $visitor_email \r\n";
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: index.html");
 ?>
